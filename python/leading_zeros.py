25fed7cd1503a4295365d6e10fc92b9df640c8a2
Cheng-Hsin <gsjh80317cla@gmail.com>
2022-08-14 16:34:31 +0800

    new version of leading_zeros.py can affect every file in the directory, and shared on GitHub

---
+++ python/leading_zeros.py
@@ -0,0 +1,62 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+
+mode_group = parser.add_mutually_exclusive_group()
+mode_group.add_argument("-p",
+                        "--pad",
+                        action = "store_true",
+                        help = "Only affect files that starts with '#'. Pad one zero after '#'")
+
+mode_group.add_argument("-d",
+                        "--delete",
+                        action = "store_true",
+                        help = "Only affect file that starts with '#0'. Deleter one zero after '#'")
+
+args = parser.parse_args()
+
+if not args.pad and not args.delete:
+    print("No specified mode. Exiting...")
+    exit(1)
+
+def pad_zero(current_directory, file, space_index):
+    if not file.startswith('#') or file.index(' ') != space_index:
+        return
+
+    new_name = '#0' + file[1:]
+    os.rename(os.path.join(current_directory, file), os.path.join(current_directory, new_name))
+
+def delete_zero(current_directory, file, space_index):
+    if not file.startswith('#0') or file.index(' ') != space_index:
+        return
+
+    new_name = '#' + file[2:]
+    os.rename(os.path.join(current_directory, file), os.path.join(current_directory, new_name))
+
+def get_space_index(files):
+    space_index = set()
+    for file in files:
+        if file.startswith('#'):
+            space_index.add(file.index(' '))
+
+    if len(space_index) == 0:
+        return 0
+
+    if args.pad:
+        return min(space_index)
+    elif args.delete:
+        return max(space_index)
+
+def loop_through_directory(directory):
+    for current_directory, _, files in os.walk(directory):
+        space_index = get_space_index(files)
+        for file in files:
+            leading_zero = pad_zero if args.pad else delete_zero
+            leading_zero(current_directory, file, space_index)
+
+def main():
+    directory = os.path.dirname(os.path.realpath(__file__))
+    loop_through_directory(directory)
+
+main()
