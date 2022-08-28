b9fbabb8aeee30d40fd4356dc90390797ffc8f7f
Cheng-Hsin <gsjh80317cla@gmail.com>
2022-08-28 22:51:24 +0800

    optimize and generalize the solution so it allocates less memory in seen and can also deel with negative inputs

--- CodeWars/C/8 kyu/#49 Remove duplicates from list.c
+++ CodeWars/C/8 kyu/#49 Remove duplicates from list.c
@@ -11,19 +11,26 @@ int* distinct(const int* values, size_t count, size_t* pResultCount)
     // in the memory pointed to by pResultCount. If values == NULL
     // or count == 0, then *pResultCount == 0.
     int max = values[0];
+    int min = values[0];
     for (size_t index = 1; index < count; ++index)
+    {
         if (values[index] > max)
             max = values[index];
 
+        if (values[index] < min)
+            min = values[index];
+    }
+
     // seen is used as a "bit mask", which makes use of the fact that calloc
     // initializes the array to zeros, or '\0' in char*
-    char* seen = (char*) calloc(max + 1, sizeof(char));
+    // "+2" is because there are max - min + 1 possible values, add one more for '\0'
+    char* seen = (char*) calloc(max - min + 2, sizeof(char));
     int* result = (int*) calloc(count, sizeof(int));
     *pResultCount = 0;
     for (size_t index = 0; index < count; ++index)
-        if (!seen[values[index]])
+        if (!seen[values[index] - min])
         {
-            seen[values[index]] = 's'; // s stands for "seen"
+            seen[values[index] - min] = 's'; // s stands for "seen"
             result[(*pResultCount)++] = values[index];
         }
     return result;
