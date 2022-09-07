2d95662779705188eae3a3b102052441e0c9f5af
Cheng-Hsin <gsjh80317cla@gmail.com>
2022-09-07 22:01:38 +0800

    add daily practice solutions 9/7

---
+++ CodeWars/C/8 kyu/#91 Check same case.c
@@ -0,0 +1,22 @@
+// Write a function that will check if two given characters are the same case.
+//
+// If either of the characters is not a letter, return -1
+// If both characters are the same case, return 1
+// If both characters are letters, but not the same case, return 0
+// Examples
+// 'a' and 'g' returns 1
+//
+// 'A' and 'C' returns 1
+//
+// 'b' and 'G' returns 0
+//
+// 'B' and 'g' returns 0
+//
+// '0' and '?' returns -1
+
+#include <ctype.h>
+
+int same_case(char a, char b)
+{
+    return (isalpha(a) && isalpha(b)) ? (isupper(a) == isupper(b)) : -1;
+}
