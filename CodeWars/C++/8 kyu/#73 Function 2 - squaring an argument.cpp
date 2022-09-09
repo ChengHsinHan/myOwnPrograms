f30988203410f406c692a5ee2839b984b5a2ca98
Cheng-Hsin <gsjh80317cla@gmail.com>
2022-09-10 01:38:13 +0800

    add daily practice solutions 9/9

---
+++ CodeWars/C++/8 kyu/#73 Function 2 - squaring an argument.cpp
@@ -0,0 +1,10 @@
+// Now you have to write a function that takes an argument and returns the
+// square of it.
+
+// Define square function... I would rather write a general one, what about you?
+
+template <typename T>
+T square(T argument)
+{
+    return argument * argument;
+}
---
+++ CodeWars/C/8 kyu/#95 Function 2 - squaring an argument.c
@@ -0,0 +1,9 @@
+// Now you have to write a function that takes an argument and returns the
+// square of it.
+
+#include <stdint.h>
+
+int32_t square(int32_t number)
+{
+    return number * number;
+}
