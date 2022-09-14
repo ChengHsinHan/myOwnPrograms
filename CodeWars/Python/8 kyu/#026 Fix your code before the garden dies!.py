4968d6fb44fee11d4064991da9b987e66283bbf7
Cheng-Hsin <gsjh80317cla@gmail.com>
2022-09-14 21:05:51 +0800

    modify previous solutions for formatting consistency reason

--- CodeWars/Python/8 kyu/#026 Fix your code before the garden dies!.py
+++ CodeWars/Python/8 kyu/#026 Fix your code before the garden dies!.py
@@ -7,7 +7,7 @@
 # Your task is to debug the code before your plants die!
 
 def rain_amount(mm):
-    if(mm < 40):
+    if (mm < 40):
         return "You need to give your plant " + str(40 - mm) + "mm of water"
     else:
         return "Your plant has had more than enough water for today!"
